48aaaab1284c03ae01ba3e52c3a2b48cc7b41f15
ashen <ashenperera550@gmail.com>
2025-03-24 10:51:10 +0530

    Updated for vowel count

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def count_characters(word):
     return len(word)
 
+def count_vowels(word):
+    vowels = "aeiouAEIOU"
+    found_vowels = [char for char in word if char in vowels]
+    return len(found_vowels), found_vowels
+
 word = input("Enter a word: ")
 print("Number of characters:", count_characters(word))
+vowel_count, vowel_list = count_vowels(word)
+print("Number of vowels:", vowel_count)
+print("Vowels found:", vowel_list)
